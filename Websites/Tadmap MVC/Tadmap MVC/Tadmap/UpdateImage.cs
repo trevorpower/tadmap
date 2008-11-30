5a6dffda61e232aee0f515dab7a728ee79a470a1
trevorpower <trevorpower@4a648790-0dd6-46be-98e2-83199854fd07>
2008-11-30 20:38:46 +0000

    Added "Tad" theme and master page to MVC application

---
+++ Websites/Tadmap MVC/Tadmap MVC/Tadmap/UpdateImage.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Web.Script.Services;
+using System.Linq;
+using System.Security;
+using TadMap;
+using TadMap.Configuration;
+using TadMap.Security;
+
+/// <summary>
+/// Summary description for UpdateTitle
+/// </summary>
+[WebService(Namespace = "http://tadmap.com/WebServices")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[ScriptService]
+public class UpdateImage : System.Web.Services.WebService
+{
+   public UpdateImage()
+   {
+      //Uncomment the following line if using designed components 
+      //InitializeComponent(); 
+   }
+
+   [WebMethod]
+   public bool UpdateTitle(string id, string title)
+   {
+      if (id == null)
+         throw new ArgumentNullException("id");
+
+      if (title == null)
+         throw new ArgumentNullException("title");
+
+      return TadImage.UpdateTitle(new Guid(id), title, HttpContext.Current.User) > 0;
+   }
+
+   [WebMethod]
+   public bool UpdateDescription(string id, string description)
+   {
+      if (id == null)
+         throw new ArgumentNullException("id");
+
+      if (description == null)
+         throw new ArgumentNullException("description");
+
+      return TadImage.UpdateDescription(new Guid(id), description, HttpContext.Current.User) > 0;
+   }
+
+   [WebMethod]
+   public bool Mark(string id)
+   {
+       if (!HttpContext.Current.User.IsInRole(TadMapRoles.Administrator))
+           throw new SecurityException("Only administrators can mark images as offensive.");
+
+       Tadmap tadmap = new Tadmap(Database.TadMapConnection);
+
+       UserImage image = tadmap.UserImages.Single(i => i.Id == new Guid(id));
+
+       image.OffensiveCount = 1;
+
+       tadmap.SubmitChanges();
+
+       return true;
+   }
+
+   [WebMethod]
+   public bool UnMark(string id)
+   {
+       if (!HttpContext.Current.User.IsInRole(TadMapRoles.Administrator))
+           throw new SecurityException("Only administrators can mark images as un-offensive.");
+
+       Tadmap tadmap = new Tadmap(Database.TadMapConnection);
+
+       UserImage image = tadmap.UserImages.Single(i => i.Id == new Guid(id));
+
+       image.OffensiveCount = 0;
+
+       tadmap.SubmitChanges();
+
+       return true;
+   }
+}
+
--- Websites/Tadmap MVC/Tadmap MVC/Views/Home/Index.aspx
+++ Websites/Tadmap MVC/Tadmap MVC/Views/Home/Index.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Tadmap_MVC.Views.Home.Index" %>
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Main.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Tadmap_MVC.Views.Home.Index" Theme="Tad" %>
 
 <asp:Content ID="indexContent" ContentPlaceHolderID="MainContent" runat="server">
     <h2><%= Html.Encode(ViewData["Message"]) %></h2>
