5d954d2a46be3ad4a7dafbbbf34c8196f3397860
trevorpower <trevorpower@4a648790-0dd6-46be-98e2-83199854fd07>
2008-12-20 11:20:28 +0000

    Added image not found exception

---
+++ Websites/Tadmap MVC/Tadmap MVC/Models/Images/ImageNotFound.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace Tadmap_MVC.Models.Images
+{
+   public class ImageNotFound : Exception
+   {
+      public ImageNotFound():base()
+      {
+      }
+   }
+}
