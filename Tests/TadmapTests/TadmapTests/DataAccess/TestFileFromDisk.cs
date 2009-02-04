842b0596ea4af2304dc29e09f78f7a35247e1fcb
trevorpower <trevorpower@4a648790-0dd6-46be-98e2-83199854fd07>
2009-02-04 22:02:06 +0000

    Added tests for security re-factor

---
+++ Tests/TadmapTests/TadmapTests/DataAccess/TestFileFromDisk.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tadmap_MVC.DataAccess;
+using System.IO;
+
+namespace TadmapTests.DataAccess
+{
+   internal class TestFileFromDisk : IUploadedFile
+   {
+      FileStream _stream;
+      string _path;
+
+      public TestFileFromDisk(string path)
+      {
+         string dir = System.Environment.CurrentDirectory;
+         _stream = new FileStream(path, FileMode.Open);
+         _path = path;
+      }
+
+      #region IUploadedFile Members
+
+      public string FileName
+      {
+         get { return Path.GetFileName(_path); }
+      }
+
+      public Stream InputStream
+      {
+         get { return _stream; }
+      }
+
+      public int ContentLength
+      {
+         get { return (int)_stream.Length; }
+      }
+
+      #endregion
+   }
+}
