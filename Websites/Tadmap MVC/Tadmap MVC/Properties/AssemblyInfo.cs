ef3bdb32865fff75b77f65e66c109e249d78454f
trevorpower <trevorpower@4a648790-0dd6-46be-98e2-83199854fd07>
2009-02-05 20:49:53 +0000

    Moved FXCop and added custom dictionary Set a language for the application DLL

--- Websites/Tadmap MVC/Tadmap MVC/Properties/AssemblyInfo.cs
+++ Websites/Tadmap MVC/Tadmap MVC/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Tadmap_MVC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Tadmap")]
 [assembly: AssemblyProduct("Tadmap_MVC")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2008")]
+[assembly: AssemblyCopyright("Copyright © Tadmap 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-IE")]
