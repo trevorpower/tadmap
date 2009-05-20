f5727883ff4ebf48c6f821f8d62cca114facf5d1
trevorpower <trevorpower@4a648790-0dd6-46be-98e2-83199854fd07>
2009-05-20 19:24:43 +0000

    Fixed some build errors and removed some redundant directories

--- Websites/Tadmap.Website/Tadmap MVC/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tadmap.TadmapApplication" Language="C#" %>
--- Websites/Tadmap.Website/Tadmap MVC/GlobalSuppresion.cs
+++
@@ -1,2 +0,0 @@
-﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:DontUseKeywordShared", MessageId="Shared", Target="Tadmap.Views.Shared")]
-[module: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:DontUseKeywordShared", MessageId="Shareed", Target="Tadmap.Views.Shared")]
