ad119cd408bbcbb6b385d530e768fed37fd12ab3
trevorpower <trevorpower@4a648790-0dd6-46be-98e2-83199854fd07>
2009-06-18 10:51:24 +0000

    Uploaded test files for Tadmap.Server

---
+++ Applications/Tadmap.Server.Test/TestImageProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Tadmap.Model.Image;
+
+namespace Tadmap.Server.Test
+{
+   [TestFixture]
+   public class TestImageProcess
+   {
+      [Test]
+      public void FullTest()
+      {
+         IBinaryRepository repository = new Tadmap.Amazon.BinaryRepository("1RYDPTK2VKP6739SPGR2", "FCbtO3UEUp7/5Fql3L57n1cA+d5OEnVP88EsDqJ7", "tadmap");
+
+         ProcessImage.Process("1e43ffaa-b2c5-4852-a0c7-90a5926b2bce.jpg", repository);
+      }
+   }
+}
---
+++ Applications/Tadmap.Server/Tadmap.Server/ProcessImage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tadmap.Model.Image;
+using System.Diagnostics;
+using System.IO;
+
+namespace Tadmap.Server
+{
+   public class ProcessImage
+   {
+      static public ImageProcessingResult Process(string imageName, IBinaryRepository binaryRepository)
+      {
+         var log = new EventLog("Application")
+         {
+            Source = "Tadmap"
+         };
+
+         log.WriteEntry("Processing image:" + imageName, EventLogEntryType.Information);
+
+         Stream binary = binaryRepository.GetBinary(imageName);
+
+         if (binary == null)
+         {
+            log.WriteEntry("No binary found:" + imageName, EventLogEntryType.Warning);
+            return new ImageProcessingResult { Key = imageName, Result = ImageProcessingResult.ResultType.Failed }; // Image not available in the queue yet.
+         }
+
+         // If I have an image I should renew the message.
+
+         IImageSet imageSet = new ImageSet1(imageName);
+
+         int zoomLevels;
+         int tileSize;
+         imageSet.Create(binary, binaryRepository, out zoomLevels, out tileSize);
+         log.WriteEntry("Processing finished.", EventLogEntryType.Information);
+
+         return new ImageProcessingResult
+         {
+            Key = imageName,
+            Result = ImageProcessingResult.ResultType.Complete,
+            ZoomLevel = zoomLevels,
+            TileSize = tileSize
+         };
+      }
+   }
+}
